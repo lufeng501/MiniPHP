2658465b6fbf93dcc5fce70843ef78b08cfe3920
lufeng501 <lufeng501@126.com>
2017-05-24 15:35:59 +0800

    数据库配置文件

---
+++ config/databases.example.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Des : 数据库配置
+ * Created by PhpStorm.
+ * User: lufeng501206@gmail.com
+ * Date: 2017/5/24
+ * Time: 11:57
+ */
+
+return [
+    'default' => 'pdo',
+    'pdo' => [
+        'host' => 'localhost',
+        'post' => '3306',
+        'user' => '',
+        'password' => '',
+        'database' => ''
+    ],
+    'mysqli' => [
+        'host' => 'localhost',
+        'post' => '3306',
+        'user' => '',
+        'password' => '',
+        'database' => ''
+    ]
+];
